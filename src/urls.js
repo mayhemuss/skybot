d4cc2aebd0ba02002f53129f469c4bdf12ba1e52
mayhemus <mayhemus@yandex.ru>
2024-08-14 22:43:24 +0300

    done

--- src/urls.js
+++ src/urls.js
@@ -1,2 +1,2 @@
-// export const BACK_URL = "https://skynetgamesbot.ru/reg"
-export const BACK_URL = "https://back.mayhemus.keenetic.pro/reg"
+export const BACK_URL = "https://skynetgamesbot.ru/reg"
+// export const BACK_URL = "https://back.mayhemus.keenetic.pro/reg"
