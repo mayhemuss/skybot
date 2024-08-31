cf2929469f8b7e3004e24c747ec4bf208a2f0e39
mayhemus <mayhemus@yandex.ru>
2024-08-31 10:53:14 +0300

    valorant start7

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import {Spinner} from "./Pages/Spinner/Spinner";
 import {Route, Routes,} from "react-router-dom";
 import {Suspense} from 'react';
-import {FifaLazy} from "./Pages/Fifa/FifaLazy";
 import Fifa from "./Pages/Fifa/Fifa";
 import Iphone from "./Pages/Iphone/Iphone";
 
@@ -11,12 +10,11 @@ function App() {
   return (
     <Suspense fallback={<Spinner/>}>
       <Routes>
-        <Route path={"/"} element={<Fifa/>}/>
+        <Route path={"/app"} element={<Fifa/>}/>
         <Route path={"/iphone"} element={<Iphone/>}/>
       </Routes>
     </Suspense>
-  )
-    ;
+  );
 }
 
 export default App;
