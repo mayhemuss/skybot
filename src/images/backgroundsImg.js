b0750d99d723e631e8ca4c04628ac26002d861d8
mayhemuss <mayhemus@yandex.ru>
2024-09-24 17:11:04 +0300

    add mlbb

--- src/images/backgroundsImg.js
+++ src/images/backgroundsImg.js
@@ -3,6 +3,6 @@ export const backgroundsImg = {
   dota: "https://sun9-28.userapi.com/impg/cywL8fuFRvo14aaOlFQIF-z7TrKn30zQAvOsmw/KBMfKd-thmY.jpg?size=1080x1920&quality=96&sign=c2e8e863b94d0c5eb7d73b3950143f3d&type=album",
   cs: "https://i.playground.ru/e/u18h2k2WwaVr3NNeYt21Gw.jpeg?600xauto",
   valorant: "https://sun9-59.userapi.com/impg/W-xL3HtDF5HcVnpouLYIE3lxZhetVgILD2Gv7A/J10CoexrvjE.jpg?size=1080x1920&quality=95&sign=7ffe836d46be98ec23c5f3ffe7444e4f&type=album",
-  iphone:"https://mtscdn.ru/upload/iblock/72b/iphone_15_black.png",
-
+  iphone: "https://mtscdn.ru/upload/iblock/72b/iphone_15_black.png",
+  mlbb: "https://sun9-64.userapi.com/impg/60-fohYNhMjQKfBrEpX0J173iAMkQbHcD24RrQ/Fg7LZlE_n_k.jpg?size=1080x1920&quality=95&sign=08e3f8642edec5f924d3058965c53ab2&type=album"
 }
