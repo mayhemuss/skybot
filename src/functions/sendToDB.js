ee52848a27581e777d2d6c1945ff1eaa3130ae0e
mayhemuss <mayhemus@yandex.ru>
2024-11-24 21:49:07 +0300

    refactor 1

--- src/functions/sendToDB.js
+++ src/functions/sendToDB.js
@@ -12,30 +12,30 @@ export const sendToDB = async (query) => {
     const data = window.Telegram.WebApp.initDataUnsafe
 
 
-      window.Telegram.WebApp.MainButton.disable()
-      window.Telegram.WebApp.MainButton.setParams({
-        color: "#888888",
-        text: "Загрузка..."
-      })
+    window.Telegram.WebApp.MainButton.disable()
+    window.Telegram.WebApp.MainButton.setParams({
+      color: "#888888",
+      text: "Загрузка..."
+    })
 
+    const registrationType = ref === data.user.id ? "capitan" : "user"
 
-      const responce = await fetch(BACK_URL + `/regis`, {
-        method: "POST",
-        body: JSON.stringify({
-          chatId: data.user.id,
-          telegramName: data.user.first_name,
-          username: data.user.username,
-          registrationType: ref === data.user.id ? "capitan" : "user",
-          ...query
-        }),
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Content-Type': 'application/json'
-        }
-      })
-      console.log(responce)
-      return responce;
-
+    const responce = await fetch(BACK_URL + `/regis`, {
+      method: "POST",
+      body: JSON.stringify({
+        chatId: data.user.id,
+        telegramName: data.user.first_name,
+        username: data.user.username,
+        registrationType: ref === -1 || ref === "-1" ? "mix" : registrationType,
+        ...query
+      }),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+      }
+    })
+    console.log(responce)
+    return responce;
 
 
   } catch (e) {
